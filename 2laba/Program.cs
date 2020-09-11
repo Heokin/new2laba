a31fbb123e55a8b72313169a1b20e1d477460c44
Heokin <apple.devip@gmail.com>
2020-09-11 23:12:49 +0300

    commit

--- 2laba/Program.cs
+++ 2laba/Program.cs
@@ -7,6 +7,9 @@ namespace laba
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("gnwgiw");
+
+            
         }
     }
 }
